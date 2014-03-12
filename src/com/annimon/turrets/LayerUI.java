e7ac20e5244e7875c8029978692ba27f542575bf
Victor <melnikukr@mail.ru>
2014-03-12 22:32:05 +0200

    Add PlanetBackground

--- src/com/annimon/turrets/LayerUI.java
+++ src/com/annimon/turrets/LayerUI.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 
 /**
@@ -25,6 +26,29 @@ public class LayerUI {
             g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25f));
             g2d.setPaint(new GradientPaint(0, 0, Color.BLACK, 0, h, Color.GREEN));
             g2d.fillRect(0, 0, w, h);
+            g2d.dispose();
+        }
+    }
+    
+    public static class PlanetBackground extends javax.swing.plaf.LayerUI<JComponent>
+                                         implements Constants {
+        
+        private final BufferedImage image;
+        
+        public PlanetBackground() {
+            image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+            Graphics g = image.createGraphics();
+            new Background().draw(g);
+            g.dispose();
+        }
+
+        @Override
+        public void paint(Graphics g, JComponent component) {
+            super.paint(g, component);
+            
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25f));
+            g2d.drawImage(image, 0, 0, null);
             g2d.dispose();
         }
     }
