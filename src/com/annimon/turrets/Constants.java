2e8a85bf907549182a5d1880aeb8e9bb403f50c8
Victor <melnikukr@mail.ru>
2014-03-13 14:58:00 +0200

    Add debug constant

--- src/com/annimon/turrets/Constants.java
+++ src/com/annimon/turrets/Constants.java
@@ -12,4 +12,7 @@ public interface Constants {
     public static final int WIDTH = 800, HEIGHT = 600;
     public static final boolean RESIZABLE = false;
     public static final Dimension SCREEN_DIMENSION = new Dimension(Constants.WIDTH, Constants.HEIGHT);
+    
+    // Parameters
+    public static final boolean DEBUG_MODE = true;
 }
