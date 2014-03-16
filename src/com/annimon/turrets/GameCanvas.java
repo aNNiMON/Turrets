1f273cd6142da221f63f2ddaebf2fb47c2d2ce65
Victor <melnikukr@mail.ru>
2014-03-16 13:17:32 +0200

    Add turrets

--- src/com/annimon/turrets/GameCanvas.java
+++ src/com/annimon/turrets/GameCanvas.java
@@ -8,10 +8,11 @@ import java.awt.image.BufferedImage;
  *
  * @author aNNiMON
  */
-public class GameCanvas extends DoubleBufferedCanvas {
+public class GameCanvas extends DoubleBufferedCanvas implements NetworkListener {
 
     private final BufferedImage background;
     private final Terrain terrain;
+    private Turret serverTurret, clientTurret;
     
     public GameCanvas() {
         background = new BufferedImage(Constants.WIDTH, Constants.HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -19,21 +20,51 @@ public class GameCanvas extends DoubleBufferedCanvas {
         
         terrain = new Terrain(Constants.WIDTH);
         terrain.generate(System.currentTimeMillis());
+        
+        serverTurret = new Turret(Turret.SERVER, terrain.getFirstBlockHeight(), terrain);
+        clientTurret = new Turret(Turret.CLIENT, terrain.getLastBlockHeight(), terrain);
     }
     
     @Override
     protected void draw(Graphics2D g) {
         g.drawImage(background, 0, 0, null);
         terrain.draw(g);
+        serverTurret.draw(g);
+        clientTurret.draw(g);
+    }
+    
+    @Override
+    public void onStatusChanged(int status, Object data) {
+        switch (status) {
+            case ON_CONNECT:
+                System.out.println("connect!");
+                break;
+        }
     }
 
     @Override
     protected void update() {
+        
     }
 
     private void initBackground() {
         final Graphics g = background.createGraphics();
         new Background().draw(g);
         g.dispose();
+    }
+
+    @Override
+    protected void mousePressed(int x, int y) {
+        clientTurret.setBarrelParams(x, Constants.HEIGHT - y);
+    }
+
+    @Override
+    protected void mouseReleased(int x, int y) {
+        clientTurret.shoot();
+    }
+
+    @Override
+    protected void mouseDragged(int x, int y) {
+        clientTurret.setBarrelParams(x, Constants.HEIGHT - y);
     }
 }
