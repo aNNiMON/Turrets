59122045c076f364a92ffdf7bbe2661d713dd2b8
Victor <melnikukr@mail.ru>
2014-03-23 23:09:16 +0200

    Add Preferences

---
+++ src/com/annimon/turrets/util/Prefs.java
@@ -0,0 +1,44 @@
+package com.annimon.turrets.util;
+
+import java.util.prefs.Preferences;
+
+/**
+ * Preferences helper class.
+ * @author aNNiMON
+ */
+public final class Prefs {
+    
+    private static final String SOUND_ENABLED = "sound_enabled";
+    private static final String LAST_IP = "last_ip";
+    
+    private static Prefs instance;
+    
+    public static synchronized Prefs getInstance() {
+        if (instance == null) {
+            instance = new Prefs();
+        }
+        return instance;
+    }
+    
+    private final Preferences prefs;
+
+    private Prefs() {
+        prefs = Preferences.userNodeForPackage(Prefs.class);
+    }
+    
+    public boolean soundEnabled() {
+        return prefs.getBoolean(SOUND_ENABLED, true);
+    }
+    
+    public void setSoundEnabled(boolean enabled) {
+        prefs.putBoolean(SOUND_ENABLED, enabled);
+    }
+    
+    public String lastIp() {
+        return prefs.get(LAST_IP, "127.0.0.1");
+    }
+    
+    public void setLastIp(String ip) {
+        prefs.put(LAST_IP, ip);
+    } 
+}
