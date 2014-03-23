effb0abea99ec9ce38da8d92b275a3af38408891
Victor <melnikukr@mail.ru>
2014-03-23 19:21:45 +0200

    Add ExceptionHandler

---
+++ src/com/annimon/turrets/util/ExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.annimon.turrets.util;
+
+import com.annimon.turrets.Constants;
+
+/**
+ * Handle exceptions.
+ * @author aNNiMON
+ */
+public class ExceptionHandler {
+    
+    private static final boolean DEBUG_MODE = Constants.DEBUG_MODE;
+    
+    public static void handle(Throwable throwable) {
+        if (DEBUG_MODE) {
+            throwable.printStackTrace();
+        }
+    }
+    
+    public static void handle(Throwable throwable, String comment) {
+        if (DEBUG_MODE) {
+            System.out.println(comment);
+            handle(throwable);
+        }
+    }
+}
